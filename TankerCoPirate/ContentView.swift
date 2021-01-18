15a3a0e4fecff646d989af2e3d0fec6149b8e90a
Sean Ogden <sean.ogden.1@us.af.mil>
2021-01-18 14:51:05 -0500

    Initial Commit

---
+++ TankerCoPirate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TankerCoPirate
+//
+//  Created by sean ogden on 1/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TankerCoPirate/TankerCoPirateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TankerCoPirateApp.swift
+//  TankerCoPirate
+//
+//  Created by sean ogden on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct TankerCoPirateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
